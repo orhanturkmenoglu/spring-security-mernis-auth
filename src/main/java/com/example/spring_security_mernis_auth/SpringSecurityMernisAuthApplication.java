6523ba1dc7b3691e1f839a717504ee98ea1ebe81
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2025-04-12 12:37:00 +0300

    initial commit -MERNIS doğrulamalı login

---
+++ src/main/java/com/example/spring_security_mernis_auth/SpringSecurityMernisAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_mernis_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMernisAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityMernisAuthApplication.class, args);
+	}
+
+}
